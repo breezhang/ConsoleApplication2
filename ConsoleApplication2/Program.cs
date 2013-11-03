ea7f41024532dcf5b8a23725e636666322c3c3a3
breezhang <breezang1877@hotmail.com>
2013-11-02 19:37:18 -0700

    xunit Test
    Demo xunit Test

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApplication2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+  
+    public class TestA
+    {
+        [Fact]
+        public void Test()
+        {
+
+            Assert.True(1>0);
+        }
+    } 
+    public class TestB
+    {
+        [Fact]
+        public void Test()
+        {
+
+            Assert.True(1>0);
+        }
+    }
+    public class TestC
+    {
+        [Fact]
+        public void Test()
+        {
+
+            Assert.True(1>0);
+        }
+    }
+}
